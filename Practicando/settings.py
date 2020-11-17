51c1e505f0f58fef34fa54c3fa4c8610d12f83e7
christiantc96 <73966884+christiantc96@users.noreply.github.com>
2020-11-17 15:22:20 -0500

    Update settings.py

--- Practicando/settings.py
+++ Practicando/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'Practicando.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'postgres',  
+        'USER':'django',  
+        'PASSWORD':'django',  
+        'HOST':'127.0.0.1',  
+        'PORT':'5432' ,
+    }  
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 """
 DATABASES = {  
     'default': {  
